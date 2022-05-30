8f3b66cf8675466200becf203326edc246d0ec24
ManuelP2 <manuelperezp22@hotmail.com>
2022-05-30 19:29:41 -0400

    Id security fix y some fixes design

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -61,7 +61,7 @@ const Header = () => {
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
       >
-        {[images.react, images.sass, images.javascript, images.html, images.node, images.git].map((circle, index) =>(
+        {[images.react, images.javascript, images.html, images.node, images.git].map((circle, index) =>(
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
